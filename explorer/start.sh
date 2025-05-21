bc8b101b2d77886de36550d599ac1390e3cc5569
neodix <namlem@gmail.com>
2025-05-21 09:44:56 +0400

    improve blockchain explorer

--- explorer/start.sh
+++ explorer/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$EXTERNAL_IP" ]; then
-  wget -O /usr/src/global.config.json http://$EXTERNAL_IP:8000/external.global.config.json
+  wget -O /usr/src/global.config.json http://$GENESIS_IP:8000/external.global.config.json
   test $? -eq 0 || { echo "Can't download http://$GENESIS_IP:8000/external.global.config.json "; exit 14; }
 elif [ "$GENESIS_IP" ]; then
   wget -O /usr/src/global.config.json http://$GENESIS_IP:8000/global.config.json
